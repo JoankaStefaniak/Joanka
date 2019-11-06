6caabad0b66dcec6f02a1077785680fa92b321ee
JoankaStefaniak <joankastefaniak@gmail.com>
2019-11-06 16:40:09 +0100

    05_zadania

---
+++ 08_wyjątki/zad4.py
@@ -0,0 +1,18 @@
+# Oblicz średnią arymetyczną z kilku liczb. Liczby będą podane przez użytkownika po przecinku.
+# Napisz funkcję, która przyjmie wartości i wyświetli średnią.
+# Program powinen być odporny na błędy użytkownika.
+# Błędów nie wyświetlaj, ale rodzaj błędu zapisz do pliku.
+
+# jak obliczyć średnia arytmetyczn
+# lista_liczb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# ilośc_liczb = len(lista_liczb)
+#
+# suma = 0
+# średnia = 0
+#
+# for liczba in lista_liczb:
+#     suma = suma + liczba
+#
+# średnia = suma/ ilośc_liczb
+# print("Srednia arytmetyczna liczb to:" + str(średnia))
+
